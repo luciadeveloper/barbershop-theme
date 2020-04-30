9ba5f6f274f9a12e2715a7b688e88c617174996d
Lucia <luciasanchezfraile@gmail.com>
2020-04-30 13:29:24 +0200

    all1 fixes on the slider and form

--- js/slider.js
+++ js/slider.js
@@ -16,10 +16,23 @@ jQuery('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
- //   autoplay:true,
 	dotsEach:true,
     autoplayTimeout:2000,
     animateIn:true,
+    nav : true, // show next and previous buttons
+    addClassActive : true, // visible items have class active
+    onInitialized: addDotButtonText,
+    onInitialized: function (event) {
+        jQuery('#owl-carousel').find('.owl-item').attr('aria-selected','false');
+        jQuery('#owl-carousel').find('.owl-item.active').attr('aria-selected','true'); // let screen readers know an item is active
+
+        // apply meta info to next and previous buttons and make them focusable
+        jQuery('#owl-carousel').find('.owl-prev').attr('role','button').attr('title','Previous');
+        jQuery('#owl-carousel').find('.owl-next').attr('role','button').attr('title','Next');
+        jQuery('#owl-carousel, .owl-prev, .owl-next').attr('tabindex','0');
+
+       // $('.owl-dots').attr('aria-hidden', 'true');
+    },
     responsive:{
         0:{
             items:1
@@ -32,3 +45,18 @@ jQuery('.owl-carousel').owlCarousel({
         }
     }
 })
+
+function addDotButtonText() {
+
+    // loop through each dot element
+    jQuery('.owl-dot').each(function() {
+        // remove old text nodes
+        jQuery(this).find('.offscreen').remove();
+
+        // grab its (zero-based) order in the series
+        let idx = $(this).index() + 1;
+
+        // append a span to the button containing descriptive text
+        jQuery(this).append('<span class="offscreen">Go to slide ' + idx + '</span>');
+    });
+}
